8545b21f4f0c88063a7a70a2d2b64bdc5f0838fb
Lukasz Szczesny <luk@mimuw.edu.pl>
2014-07-14 10:39:13 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+setup(
+    name='MFRC522',
+    py_modules=['MFRC522'],
+    version='0.0.1',
+    description='A small module to interface with the NFC reader Module MFRC522',
+    platforms=['Linux'],
+)
