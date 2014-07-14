d81172fec3b99ebc9919f127628f83c362776103
Lukasz Szczesny <luk@mimuw.edu.pl>
2014-07-14 10:27:19 +0200

    pep8

--- examples/write.py
+++ examples/write.py
@@ -7,8 +7,11 @@ import signal
 
 continue_reading = True
 
-# Capture SIGINT for cleanup when the script is aborted
-def end_read(signal,frame):
+
+def end_read(signal, frame):
+    """
+    Capture SIGINT for cleanup when the script is aborted
+    """
     global continue_reading
     print "Ctrl+C captured, ending read."
     continue_reading = False
@@ -20,33 +23,35 @@ signal.signal(signal.SIGINT, end_read)
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
 
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+# This loop keeps checking for chips. If one is near it will get the UID
+# and authenticate
 while continue_reading:
-    
-    # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # Scan for cards
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
     if status == MIFAREReader.MI_OK:
         print "Card detected"
-    
+
     # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+    (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
 
         # Print UID
-        print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
-    
+        print "Card read UID: " + str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3])
+
         # This is the default key for authentication
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
+        key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
         # Select the scanned tag
         MIFAREReader.MFRC522_SelectTag(uid)
 
         # Authenticate
-        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+        status = MIFAREReader.MFRC522_Auth(
+            MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
         print "\n"
 
         # Check if authenticated
@@ -56,7 +61,7 @@ while continue_reading:
             data = []
 
             # Fill the data with 0xFF
-            for x in range(0,16):
+            for x in range(0, 16):
                 data.append(0xFF)
 
             print "Sector 8 looked like this:"
@@ -76,7 +81,7 @@ while continue_reading:
 
             data = []
             # Fill the data with 0x00
-            for x in range(0,16):
+            for x in range(0, 16):
                 data.append(0x00)
 
             print "Now we fill it with 0x00:"
